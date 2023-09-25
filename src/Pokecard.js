78d9b732f810e377a1155b7cf6c2141a30c98c84
Nathan Tjahjadi <nathantja@yahoo.com>
2023-09-25 18:06:14 -0500

    styling added to pokecard and pokedex

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,14 @@
+import styles from "./Pokecard.css"
+
 const POKEMON_IMG_BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
 function Pokecard({id, name, type, base_experience}) {
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className="Pokecard">
+      <h1 className="Pokecard-name">{name}</h1>
       <img src={`${POKEMON_IMG_BASE_URL}${id}.png`} alt="pokemon" />
-      <p>Type: {`${type}`}</p>
-      <p>EXP: {`${base_experience}`}</p>
+      <p><b>Type:</b> {`${type}`}</p>
+      <p><b>EXP:</b> {`${base_experience}`}</p>
     </div>
   )
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,8 +1,16 @@
 import Pokecard from "./Pokecard";
+import styles from "./Pokedex.css";
 
 function Pokedex({ pokemons }) {
-  return pokemons.map(pokemon => <Pokecard id={pokemon.id} name={pokemon.name}
-    type={pokemon.type} base_experience={pokemon.base_experience} />)
+  return (
+    <div className="Pokedex">
+      {pokemons.map(pokemon => <Pokecard
+        id={pokemon.id}
+        name={pokemon.name}
+        type={pokemon.type}
+        base_experience={pokemon.base_experience} />)}
+    </div>
+  );
 }
 
 export default Pokedex;
